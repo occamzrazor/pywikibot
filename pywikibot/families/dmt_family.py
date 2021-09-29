ca096aaa13ba9e5d9c744886649dc24c992fbbef
Daniel Salinas <daniel@occamzrazor.com>
2021-09-29 10:41:43 -0500

    Add dmt family

---
+++ pywikibot/families/dmt_family.py
@@ -0,0 +1,8 @@
+from pywikibot import family
+
+class Family(family.Family):
+    name = 'dmt'
+    langs = {
+        'en': 'wiki.dmt-nexus.me',
+    }
+
---
+++ user-config.py
@@ -0,0 +1,8 @@
+# This is a sample file. You should use generate_user_files.py
+# to create your user-config.py file.
+
+mylang = 'en'
+family = 'dmt'
+usernames['dmt']['en'] = 'OccamzRazor'
+
+family_files['dmt'] = 'https://wiki.dmt-nexus.me/w/api.php'
